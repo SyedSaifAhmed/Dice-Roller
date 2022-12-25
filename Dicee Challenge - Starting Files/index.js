c59a29948b262fa53be4db2b64ecc36e06f0c6f1
Syed Saif Ahmed <syed.saifahmed@yahoo.com>
2022-12-25 15:46:08 +0530

    random feature added

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,3 +1,7 @@
-let randomNum = Math.floor((Math.random()*6)+1); // 1 to 6
+let randomNumber1 = Math.floor((Math.random()*6)+1); // 1 to 6
+let randomNumber2 = Math.floor((Math.random()*6)+1); // 1 to 6
 
-console.log(randomNum);
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png")
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png")
+
+// console.log(randomNumber1);
