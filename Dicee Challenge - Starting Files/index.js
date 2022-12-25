f3319ef75a9149325519e38773f29bb365aa75f9
Syed Saif Ahmed <syed.saifahmed@yahoo.com>
2022-12-25 16:20:54 +0530

    player status  added

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,7 +1,27 @@
-let randomNumber1 = Math.floor((Math.random()*6)+1); // 1 to 6
+
+////Change variable name//////
+
+let randomNumber1 = Math.floor((Math.random()*6)+1); // 1 to 6 
 let randomNumber2 = Math.floor((Math.random()*6)+1); // 1 to 6
+
+////////
 
 document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png")
 document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png")
 
 // console.log(randomNumber1);
+
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+}
+
+else if (randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+}
+
+else {
+    document.querySelector("h1").innerHTML="Draw!!";
+}
+
